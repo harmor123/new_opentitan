fea445240d9927308a2cca3e0ec1af7699fcb709
harmor123 <2678102521@qq.com>
2026-07-17 20:36:18 +0800

    fix(ver0)：ML-KEM .s文件缺少.section .text导致代码进入.data段: 全部添加

--- test_hybrid_kem_otbn_prompt_ver0/otbn/mlkem768/mlkem_keypair.s
+++ test_hybrid_kem_otbn_prompt_ver0/otbn/mlkem768/mlkem_keypair.s
@@ -1,6 +1,7 @@
 /* Copyright "Towards ML-KEM & ML-DSA on OpenTitan" Authors */
 /* Licensed under the Apache License, Version 2.0, see LICENSE for details. */
 /* SPDX-License-Identifier: Apache-2.0 */
+.section .text
 
 /*
  * Name:        indcpa_keypair
