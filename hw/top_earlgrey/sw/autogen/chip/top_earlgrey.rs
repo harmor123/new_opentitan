005098bc7431a446ec5d7d8c730ebaebf7393a88
harmor123 <2678102521@qq.com>
2026-09-04 20:59:53 +0800

    恢复内存扩展

--- hw/top_earlgrey/sw/autogen/chip/top_earlgrey.rs
+++ hw/top_earlgrey/sw/autogen/chip/top_earlgrey.rs
@@ -501,7 +501,7 @@ pub const KMAC_SIZE_BYTES: usize = 0x1000;
 ///
 /// This should be used with #mmio_region_from_addr to access the memory-mapped
 /// registers associated with the peripheral (usually via a DIF).
-pub const OTBN_BASE_ADDR: usize = 0x41300000;
+pub const OTBN_BASE_ADDR: usize = 0x41130000;
 
 /// Peripheral size for otbn in top earlgrey.
 ///
@@ -509,7 +509,7 @@ pub const OTBN_BASE_ADDR: usize = 0x41300000;
 /// memory-mapped registers associated with this peripheral should have an
 /// address between #OTBN_BASE_ADDR and
 /// `OTBN_BASE_ADDR + OTBN_SIZE_BYTES`.
-pub const OTBN_SIZE_BYTES: usize = 0x40000;
+pub const OTBN_SIZE_BYTES: usize = 0x10000;
 
 /// Peripheral base address for keymgr_dpe in top earlgrey.
 ///
--- hw/top_earlgrey/sw/autogen/top_earlgrey_memory.h
+++ hw/top_earlgrey/sw/autogen/top_earlgrey_memory.h
@@ -680,7 +680,7 @@
  * This should be used with #mmio_region_from_addr to access the memory-mapped
  * registers associated with the peripheral (usually via a DIF).
  */
-#define TOP_EARLGREY_OTBN_BASE_ADDR 0x41300000
+#define TOP_EARLGREY_OTBN_BASE_ADDR 0x41130000
 
 /**
  * Peripheral size for otbn in top earlgrey.
@@ -690,7 +690,7 @@
  * address between #TOP_EARLGREY_OTBN_BASE_ADDR and
  * `TOP_EARLGREY_OTBN_BASE_ADDR + TOP_EARLGREY_OTBN_SIZE_BYTES`.
  */
-#define TOP_EARLGREY_OTBN_SIZE_BYTES 0x40000
+#define TOP_EARLGREY_OTBN_SIZE_BYTES 0x10000
 /**
  * Peripheral base address for keymgr_dpe in top earlgrey.
  *
